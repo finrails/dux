eab1951efbf85a549bd3e9f9f03e3f25d698b1d7
Vitor Souza <desenvolvedor.vitorsouza@gmail.com>
2024-02-02 15:19:20 -0400

    add: repl evaluation

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -2,6 +2,7 @@ package repl
 
 import (
 	"bufio"
+	"dux/src/evaluator"
 	"dux/src/lexer"
 	"dux/src/parser"
 	"fmt"
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
