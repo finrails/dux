ecf2590e13c2e0e86b52da1817c2d825dd6878e9
Vitor Souza <desenvolvedor.vitorsouza@gmail.com>
2024-02-25 17:59:59 -0400

    add: basic file handling

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"os/user"
-	"os"
+	"dux/evaluator"
+	"dux/lexer"
+	"dux/object"
+	"dux/parser"
 	"dux/repl"
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
 )
 
 func main() {
-	user, err := user.Current()
+	args := os.Args[1:]
 
-	if err != nil {
-		panic(err)
+	if len(args) == 0 {
+		user, err := user.Current()
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Welcome %s. Dux Language Interpreter!\n", user.Username)
+		fmt.Printf("You can evalute Dux commands here\n");
+
+		repl.Start(os.Stdin, os.Stdout)
+	} else {
+		absp, err := filepath.Abs(args[0])
+		if err != nil { fmt.Println("Error:", err); return }
+
+		content, err := os.ReadFile(absp)
+		if err != nil { fmt.Println("Error:", err); return }
+
+		l := lexer.New(string(content))
+		p := parser.New(l)
+		program := p.ParseProgram()
+		env := object.NewEnvironment()
+
+		evaluated := evaluator.Eval(program, env)
+
+		fmt.Print(evaluated.Inspect())
 	}
-
-	fmt.Printf("Welcome %s. Dux Language Interpreter!\n", user.Username)
-	fmt.Printf("You can evalute Dux commands here\n");
-
-	repl.Start(os.Stdin, os.Stdout)
 }
--- object/object.go
+++ object/object.go
@@ -89,7 +89,7 @@ func (h *Hash) Inspect() string {
 		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
 	}
 
-	out.WriteString("{ ")
+	out.WriteString("{")
 	out.WriteString(strings.Join(pairs, ", "))
 	out.WriteString("}")
 
